dcde51b2a98c4316d05e8c423070574a4257afbd
Rizwan khan <khanrizwan5@gmail.com>
2022-07-15 15:51:15 +0530

    My 3rd commit

--- Khan.py
+++ Khan.py
@@ -1,3 +1,4 @@
 # python program to print "Hello World"
 print("Hello World")
 print("I am adding this file")
+print("I am learning Git with Github")
--- Test.java
+++ Test.java
@@ -3,5 +3,6 @@ public class Test
     public static void main(String[] args) {
         
         System.out.println("Hello World");
+        System.out.println("This code is for GitHub use only");
     }
 }
